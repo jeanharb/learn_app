7231ca253910fadead19611bd7c3e5d31e686fcd
jeanharb <jharb3@gmail.com>
2013-07-23 02:20:34 -0400

    completed header

--- app/assets/javascripts/myjs.js
+++ app/assets/javascripts/myjs.js
@@ -1,16 +1,49 @@
 $(document).ready(function(){
 	$('#signin-menu').hide();
+	$('.dropdown-menu').hide();
 });
 
 $(function(){
 	$("#signin").click(function() {
 		if ($('#signin-menu').is('.selected')){
 			$('#signin-menu').slideUp('fast');
+			$('#signin').removeClass('selected-btn');
 			$('#signin-menu').removeClass('selected');
 		}
 		else {
 			$('#signin-menu').slideDown('fast');
+			$('#signin').addClass('selected-btn');
 			$('#signin-menu').addClass('selected');
+		}
+	});
+});
+
+$(function(){
+	$(".dropdown").click(function() {
+		if ($('.dropdown-menu').is('.selected')){
+			$('.dropdown-menu').slideUp('fast');
+			$('.dropdown').removeClass('selected-btn');
+			$('.dropdown-menu').removeClass('selected');
+		}
+		else {
+			$('.dropdown-menu').slideDown('fast');
+			$('.dropdown').addClass('selected-btn');
+			$('.dropdown-menu').addClass('selected');
+		}
+	});
+});
+
+$(function(){
+	$("#body-signin").click(function() {
+		if ($('#signin-menu').is('.selected')){
+			$('#signin-menu').slideUp('fast');
+			$('#signin').removeClass('selected-btn');
+			$('#signin-menu').removeClass('selected');
+		}
+		if ($('.dropdown-menu').is('.selected')){
+			$('.dropdown-menu').slideUp('fast');
+			$('.dropdown').removeClass('selected-btn');
+			$('.dropdown-menu').removeClass('selected');
 		}
 	});
 });
