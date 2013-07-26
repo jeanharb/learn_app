a63d26bdcf985749a01403ac5b9a980c11f4bf9c
jeanharb <jharb3@gmail.com>
2013-07-26 02:18:34 -0400

    longer database

--- db/migrate/20130725200642_create_notes.rb
+++ db/migrate/20130725200642_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-      t.string :content
+      t.text :content
       t.integer :user_id
 
       t.timestamps
