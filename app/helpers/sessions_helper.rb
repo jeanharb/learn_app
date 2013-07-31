050c620e614b6d4626f61115c74d746b313d6c70
jeanharb <jharb3@gmail.com>
2013-07-30 22:04:33 -0400

    added Programs db 2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -63,6 +63,10 @@ module SessionsHelper
 			current_user?(User.find(Program.find(params[:id]).user_id)) || is_admin?
 		end
 
+		def progcreate
+			current_user?(User.find(Program.find(params[:id]).user_id))
+		end
+
 		def courcreator
 			current_user?(User.find(Course.find(params[:id]).user_id))
 		end
