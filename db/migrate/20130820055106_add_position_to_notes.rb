b9bde2249bea6540c092209986d1e0ae1350774f
jeanharb <jharb3@gmail.com>
2013-08-20 16:05:14 -0400

    removed index function in new notes column

--- db/migrate/20130820055106_add_position_to_notes.rb
+++ db/migrate/20130820055106_add_position_to_notes.rb
@@ -1,15 +1,6 @@
 class AddPositionToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :position, :integer
-
-    @courses = Course.find(:all)
-    @courses.each do |course|
-    	index = 0
-    	course.notes.each do |note|
-    		note.position = index
-    		note.save
-    		index += 1
-    	end
     end
   end
 end
