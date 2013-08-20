f519e006ba522844487d8d7dab4e14ad53001556
jeanharb <jharb3@gmail.com>
2013-08-19 22:12:54 -0400

    fixed added column

--- db/migrate/20130819230559_add_file_title_to_notes.rb
+++ db/migrate/20130819230559_add_file_title_to_notes.rb
@@ -1,12 +1,6 @@
 class AddFileTitleToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :file_title, :string
-
-    notes = Note.find(:all)
-
-    notes.each do |u|
-       u.file_title = u.filename
-       u.save
     end
   end
 end
