0a3d0c10a18654025d7c19c2facf2dd6073c8def
jeanharb <jharb3@gmail.com>
2013-08-20 20:35:47 -0400

    added course position 2

--- db/migrate/20130821000746_add_position_to_relationships.rb
+++ db/migrate/20130821000746_add_position_to_relationships.rb
@@ -1,14 +1,5 @@
 class AddPositionToRelationships < ActiveRecord::Migration
   def change
     add_column :relationships, :position, :integer
-    @programs = Program.find(:all)
-    @programs.each do |program|
-      index = 0
-      program.relationships.each do |course|
-        course.position = index
-        course.save
-        index += 1
-      end
-    end
   end
 end
